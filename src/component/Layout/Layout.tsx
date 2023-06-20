f813f2ae2269c3c498a71634440d1bfc65430fe6
Huy Bui <quochuy270800@gmail.com>
2023-06-20 15:13:03 +0300

    add layout component

---
+++ src/component/Layout/Layout.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from './Layout.module.css';
+import Header from './Header';
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const Layout = ({ children }: Props) => {
+  return (
+    <div className={styles.app__container}>
+      <Header />
+      <main className={styles.main__content}>{children}</main>
+      {/* <Footer /> */}
+    </div>
+  );
+};
+
+export default Layout;
