0a93f8685d3ea29ad20d7908420361c64a0b97fb
Huy Bui <quochuy270800@gmail.com>
2023-06-22 16:14:00 +0300

    add fetchWeatherForcase endpoint to weatherApi

--- src/store/service/service.ts
+++ src/store/service/service.ts
@@ -1,5 +1,6 @@
 // Need to use the React-specific entry point to import createApi
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import moment from 'moment';
 
 // Define a service using a base URL and expected endpoints
 export const weatherApi = createApi({
@@ -7,7 +8,31 @@ export const weatherApi = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: import.meta.env.REACT_APP_WEATHER_API,
   }),
-  endpoints: (builder) => ({}),
+  endpoints: (builder) => ({
+    fetchWeatherForcast: builder.query<unknown, string>({
+      query: (query: string) => ({
+        method: 'GET',
+        url: `forecast?latitude=65.01&longitude=25.50${query}`,
+      }),
+      transformResponse: (response: any) => {
+        const currentIndex = moment().weekday();
+        const adjustedMondayIndex = ((currentIndex + 6) % 7) + 1;
+        const rearrangedDaily: unknown = {};
+        for (const key in response.daily) {
+          rearrangedDaily[key] = [
+            ...response.daily[key].slice(adjustedMondayIndex),
+            ...response.daily[key].slice(0, adjustedMondayIndex),
+          ];
+        }
+        const rearrangedData = {
+          ...response,
+          daily: rearrangedDaily,
+        };
+        console.log(rearrangedData);
+        return rearrangedData;
+      },
+    }),
+  }),
 });
 
-export const {} = weatherApi;
+export const { useFetchWeatherForcastQuery } = weatherApi;
