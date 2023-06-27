902d8df84205c11b89f229e21657c11a5b83adfb
Huy Bui <quochuy270800@gmail.com>
2023-06-27 18:45:35 +0300

    config build path for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
 
 export default defineConfig({
+  build: {
+    outDir: 'build',
+  },
   plugins: [reactRefresh()],
   envPrefix: 'REACT_APP_',
 });
